bcb2a66c17426ae192fe48d6cd90604ac81c2c4f
Blair <120442257+cocos-beans@users.noreply.github.com>
2023-11-03 12:18:24 -0500

    Add files via upload

---
+++ experiment/index.js
@@ -0,0 +1,7 @@
+const input = document.getElementById('uploadAudio')
+let preview = document.getElementsByClassName('preview')
+
+function update() {
+    preview.innerHTML = "yes"
+    console.log(preview.innerHTML)
+}
